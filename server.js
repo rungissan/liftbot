50f52d2f86371a5b3ff3ba855b109cce219238b2
iurii <yury@samoshk.in>
2017-08-20 13:52:58 +0300

    4

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: '73a78204-a51b-481e-af2a-cb72c1c00de3',
+    appPassword: 'ACjQzDRONDUOTcdYRcTZruv'
 });
 
 // Listen for messages from users 
